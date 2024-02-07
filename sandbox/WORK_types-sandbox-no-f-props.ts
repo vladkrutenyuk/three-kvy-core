947e2aaeffed149b11bc1eb4ca729a870fd7ce65
kvy <vladislove2402@gmail.com>
2024-02-07 12:46:28 +0300

    compatible feature workable checkpoint

--- sandbox/WORK_types-sandbox-no-f-props.ts
+++ sandbox/WORK_types-sandbox-no-f-props.ts
@@ -1,150 +1,71 @@
-abstract class GameWorldModule {}
+export {};
 
-type GameWorldModulesRecord = Readonly<Record<string, GameWorldModule>>;
+type ModulesRecord = Readonly<Record<string, any>>;
 
-// Условный тип для проверки совместимости модулей
-type CompatibleFeature<
-	TGameObjectModules extends GameWorldModulesRecord,
-	TFeatureModules extends GameWorldModulesRecord
-> = keyof TFeatureModules extends keyof TGameObjectModules
-	? Feature<TFeatureModules>
-	: never;
-
-type isPart<
-	TFeature extends Feature<TPart>,
-	TPart extends GameWorldModulesRecord,
-	TMain extends GameWorldModulesRecord,
+type isSubsetRecord<
+	TSub extends Record<string, any>,
+	TRecord extends Record<string, any>
 > = {
-	[K in keyof TPart]: K extends keyof TMain
-		? TPart[K] extends TMain[K]
+	[K in keyof TSub]: K extends keyof TRecord
+		? TSub[K] extends TRecord[K]
 			? never
 			: K
 		: K;
-}[keyof TPart] extends never
-	? Feature<TPart>
-	: never;
+}[keyof TSub];
 
-type _CompatibleFeature<
-	TGameObjectModules extends GameWorldModulesRecord,
-	TFeature
-> = TFeature extends Feature<infer U>
-	? U extends GameWorldModulesRecord
-		? IsSubset<U, TGameObjectModules> extends true
-			? TFeature
-			: never
-		: never
-	: never;
+// Условный тип для проверки совместимости модулей
+type CompatibleFeature<
+	TFeature extends Feature<TPart>,
+	TPart extends ModulesRecord,
+	TMain extends ModulesRecord
+> = isSubsetRecord<TPart, TMain> extends never ? Feature<TPart> : never;
 
-type IsSubset1<A extends Record<string, any>, B extends Record<string, any>> = {
-	[K in keyof A]: K extends keyof B ? (A[K] extends B[K] ? never : K) : K;
-}[keyof A];
-
-type isSubsetWrapper<
-	A extends Record<string, any>,
-	B extends Record<string, any>
-> = IsSubset1<A, B> extends never ? A : never;
-
-type IsSubset<T, U> = {
-	[K in keyof T]: K extends keyof U ? (T[K] extends U[K] ? true : false) : false;
-}[keyof T] extends true
-	? true
-	: false;
-
-class GameObject<TModules extends GameWorldModulesRecord = {}> {
+class GameObject<TModules extends ModulesRecord = {}> {
 	addFeature<
 		TFeature extends Feature<TFeatureModules>,
-		TFeatureModules extends GameWorldModulesRecord = {}
+		TFeatureModules extends ModulesRecord = {}
 	>(
-		feature: new (p: FeatureProps) => isPart<TFeature, TFeatureModules, TModules>
-	): isPart<TFeature, TFeatureModules, TModules> {
-		const instance = new feature({ gameObject: this });
+		feature: new (gameObject: GameObject<TModules>) => CompatibleFeature<
+			TFeature,
+			TFeatureModules,
+			TModules
+		>
+	): CompatibleFeature<TFeature, TFeatureModules, TModules> {
+		const instance = new feature(this);
 		return instance;
 	}
-
-	_addFeature<
-		TFeature extends Feature<isSubsetWrapper<TFeatureModules, TModules>>,
-		TFeatureModules extends GameWorldModulesRecord
-	>(feature: new (p: FeatureProps) => TFeature): TFeature {
-		return new feature({ gameObject: this });
-	}
 }
 
-function isNever<T>(value: T): value is never {
-	return !value;
-}
-
-type FeatureProps = {
-	gameObject: GameObject<any>;
-};
-
-abstract class Feature<TModules extends GameWorldModulesRecord = {}> {
+abstract class Feature<TModules extends ModulesRecord = {}> {
 	gameObject: GameObject<TModules>;
-	constructor(props: FeatureProps) {
-		this.gameObject = props.gameObject;
+	constructor(gameObject: GameObject<any>) {
+		this.gameObject = gameObject;
 	}
 }
 
 // examples
 
-class PhysicsModule extends GameWorldModule {}
-class ParticleModule extends GameWorldModule {}
-
-class SimpleFeature extends Feature {}
-class PhysicalFeature extends Feature<{ physics: PhysicsModule }> {}
-class ParticlesFeature extends Feature<{ particles: ParticleModule }> {}
+class Feature0 extends Feature {}
+class FeatureA extends Feature<{ a: string }> {}
+class FeatureB extends Feature<{ b: number }> {}
 
 // initialization
-let go1 = new GameObject();
-let go2 = new GameObject<{ physics: PhysicsModule }>();
-let go3 = new GameObject<{ physics: PhysicsModule; particles: ParticleModule }>();
-let go4 = new GameObject<{ physics: string; particles: ParticleModule }>();
+let go0 = new GameObject();
+let goA = new GameObject<{ a: string }>();
+let goAB = new GameObject<{ a: string; b: number }>();
+let goABbad = new GameObject<{ a: string; b: string }>();
 
 // works but bad return types
-let ph1 = go1.addFeature(SimpleFeature); // ОК
-// let s1 = go1.addFeature(PhysicalFeature); // ОК ОШИБКА
-// let pa1 = go1.addFeature(ParticlesFeature); // ОК ОШИБКА
+let ph1 = go0.addFeature(Feature0); // ОК
+let s1 = go0.addFeature(FeatureA); // ОШИБКА
+let pa1 = go0.addFeature(FeatureB); // ОШИБКА
 
-let ph2 = go2.addFeature(PhysicalFeature); // ОК
-let s2 = go2.addFeature<SimpleFeature>(SimpleFeature); // ОК
-// let pa2 = go2.addFeature(ParticlesFeature); // ОК ОШИБКА
+let ph2 = goA.addFeature(FeatureA); // ОК
+let s2 = goA.addFeature<Feature0>(Feature0); // ОК
+let pa2 = goA.addFeature(FeatureB); // ОШИБКА
 
-let ph3 = go3.addFeature(PhysicalFeature); // ОК
-let s3 = go3.addFeature(SimpleFeature); // ОК
-let pa3 = go3.addFeature(ParticlesFeature); // ОК
+let ph3 = goAB.addFeature(FeatureA); // ОК
+let s3 = goAB.addFeature(Feature0); // ОК
+let pa3 = goAB.addFeature(FeatureB); // ОК
 
-// let ph4 = go4.addFeature(PhysicalFeature); // не ок - должна быть ошибка
-
-// works but no error on return never
-let _ph1 = go1._addFeature(SimpleFeature); // ОК
-let _s1 = go1._addFeature(PhysicalFeature); // ОК ОШИБКА
-let _pa1 = go1._addFeature(ParticlesFeature); // ОК ОШИБКАт
-
-let _ph2 = go2._addFeature(PhysicalFeature); // ОК
-let _s2 = go2._addFeature(SimpleFeature); // ОК
-let _pa2 = go2._addFeature(ParticlesFeature); // ОК ОШИБКА
-
-let _ph3 = go3._addFeature(PhysicalFeature); // ОК
-let _s3 = go3._addFeature(SimpleFeature); // ОК
-let _pa3 = go3._addFeature(ParticlesFeature); // ОК
-
-let _ph4 = go4._addFeature(PhysicalFeature); // ОК
-
-// let _ph1 = go1._addFeature<{}, SimpleFeature>(SimpleFeature); // SimpleFeature
-// let _s1 = go1._addFeature<{ physics: PhysicsModule }, PhysicalFeature>(PhysicalFeature); // never
-// let _pa1 = go1._addFeature<{ particles: ParticleModule }, ParticlesFeature>(
-// 	ParticlesFeature
-// ); // never
-
-// let _ph2 = go2._addFeature<{ physics: PhysicsModule }, PhysicalFeature>(PhysicalFeature); // PhysicalFeature
-// let _s2 = go2._addFeature<{}, SimpleFeature>(SimpleFeature); // SimpleFeature
-// let _pa2 = go2._addFeature<{ particles: ParticleModule }, ParticlesFeature>(
-// 	ParticlesFeature
-// ); // never
-
-// let _ph3 = go3._addFeature<{ physics: PhysicsModule }, PhysicalFeature>(PhysicalFeature); // PhysicalFeature
-// let _s3 = go3._addFeature(SimpleFeature); // never
-// let _pa3 = go3._addFeature<{ particles: ParticleModule }, ParticlesFeature>(
-// 	ParticlesFeature
-// ); // ParticlesFeature
-
-export {};
+let ph4 = goABbad.addFeature(FeatureB); // ОШИБКА
